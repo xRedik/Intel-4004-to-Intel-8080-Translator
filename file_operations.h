e95fc46aeeb8956d61eb57ee497081155c5c8c6e
Farid <feridquliyev8@gmail.com>
2022-12-05 02:57:09 -0500

    dict was erased

--- file_operations.h
+++ file_operations.h
@@ -9,7 +9,6 @@ using namespace std;
 class Intel4004_file_op{
 
 	private:
-		unordered_map <string,int> umap_ins;
 		vector <string> arr_ins{
 			"nop","jcn","fim","jin","jun","jms","inc","isz",
 			"add","sub","ld","xch","bbl","ldm","wrm","wmp",
@@ -24,17 +23,6 @@ class Intel4004_file_op{
 
 	Intel4004_file_op(string filename){
 
-		//initialize umap
-		umap_ins = {{"nop",0},{"jcn",1},{"fim",1},{"jin",1},{"jun",1},
-			    {"jms",1},{"inc",1},{"isz",1},{"add",1},{"sub",1},
-			    {"ld",1},{"xch",1},{"bbl",1},{"ldm",1},{"wrm",1},
-			    {"wmp",1},{"wrr",1},{"wr0",1},{"wr1",1},{"wr2",1},
-			    {"wr3",1},{"sbm",1},{"rdm",1},{"rdr",1},{"adm",1},
-			    {"rd0",1},{"rd1",1},{"rd2",1},{"rd3",1},{"clb",1},
-			    {"clc",1},{"iac",1},{"cmc",1},{"cma",1},{"ral",1},
-			    {"rar",1},{"tcc",1},{"dac",1},{"tcs",1},{"stc",1},
-			    {"daa",1},{"kbp",1},{"dcl",1},{"src",1},{"fin",1},
-		};
 
 		ifstream intel4004_file(filename);
 
---
+++ new_assembly.asm
@@ -0,0 +1,10 @@
+CMC
+NOP ;this is a comment as well 
+CMC
+NOP ;this is a comment 
+
+loop:
+ MVI a, 5h
+ INR b
+ INR c
+MVI b, c0h
